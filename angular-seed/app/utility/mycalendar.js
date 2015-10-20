29e5ffa0e1f2ae0c88ea50e25a8cb7e56e47c872
nstutu <linyzy@msn.com>
2015-10-20 22:02:01 +0800

    calendar go on
    as title

--- angular-seed/app/utility/mycalendar.js
+++ angular-seed/app/utility/mycalendar.js
@@ -6,19 +6,26 @@ controller('calCtrl', ['$scope', function($scope){
 	$scope.items=[
 		{"c":"d1","t":"1"},
 		{"c":"d2","t":"0"},
-		{"c":"d3","t":"0"},
+		{"c":"d3","t":"99"},
 		{"c":"d4","t":"0"},
 		{"c":"d5","t":"0"},
 		{"c":"d6","t":"0"},
 		{"c":"d7","t":"0"},								
 	];	
-	$scope.date = getMydate();
+	$scope.date = getMydate(0);
+	$scope.varMonth = 0;
+	$scope.addMonth = function(m){
+		$scope.varMonth+=m;
+		$scope.date=getMydate($scope.varMonth);
+	};
 
-	function getMydate(){
+
+	function getMydate(m){
 		var d= new Date();
-		var year = d.getFullYear();
-		var month = d.getMonth();
+		var tempMonth = d.getMonth();
+		var month = d.getMonth(d.setMonth(tempMonth+m))+1;
 		var day= d.getDay();
+		var year = d.getFullYear();
 		console.log(year + '年' + month + '月');
 		return year + '年' + month + '月';
 	};
